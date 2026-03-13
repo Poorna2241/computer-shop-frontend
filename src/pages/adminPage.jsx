fde84c45b934a7ee26cf45d6a5df5990fa560b12
Poorna Sachinthana <poornasachinthana2241@gmail.com>
2026-03-13 15:46:57 +0530

    Update admin page

--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -1,51 +1,89 @@
-import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import HomePage from "./homePage";
-import { LuClipboardList } from "react-icons/lu";
-import { LuBoxes } from "react-icons/lu";
-import { LuUsers } from "react-icons/lu";
-import { MdOutlineRateReview } from "react-icons/md";
-import AdminProductsPage from "./admin/adminProductsPage.jsx";
-import AdminAddProductPage from "./admin/adminAddProduct.jsx";
-import AdminUpdateProductPage from "./admin/adminUpdateProductPage.jsx";
-import AdminOrdersPage from "./admin/adminOrdersPage.jsx";
-
-
+import { LuBoxes, LuClipboardList } from "react-icons/lu";
+import { FiUsers } from "react-icons/fi";
+import AdminProductsPage from "./admin/adminProductsPage";
+import AdminAddProductPage from "./admin/adminAddProductPage";
+import AdminUpdateProductPage from "./admin/adminUpdateProductPage";
+import AdminOrdersPage from "./admin/adminOrdersPage";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Loader from "../components/loader";
+import AdminUsersPage from "./admin/adminUsersPage";
 export default function AdminPage() {
+    const [user, setUser] = useState(null);
 
-    return(
-    <div className="w-full h-full flex bg-accent">
-        <div className="w-[300px] h-full bg-accent">
-
-            <div className="w-full h-[100px] text-primary flex items-center text-2xl">
-                <img src="/logo.png" alt="Logo" className="h-full"/>
-                <h1>Admin </h1>
-
-            </div>
-
-            <div className="w-full h-[400px]  text-primary text-2xl flex flex-col pl-[20px] pt-[20px] gap-[20px] ">
-
-                
-                <Link to="/admin/orders" className="w-full flex items-center h-[30px] gap-[10px]"><LuClipboardList />Orders</Link>
-                <Link to="/admin/products" className="w-full flex items-center h-[30px] gap-[10px]"><LuBoxes />Products</Link>
-                <Link to="/admin/users" className="w-full flex items-center h-[30px] gap-[10px]"><LuUsers />Users</Link>
-                <Link to="/admin/reviews" className="w-full flex items-center h-[30px] gap-[10px]"><MdOutlineRateReview />Reviews</Link>
-            </div>
-
-        </div>
-
-        <div className=" w-[calc(100%-300px)] h-full max-h-full bg-primary overflow-y-scroll border-4 rounded-3xl border-accent"> 
-            {/* <div className="w-full h-full max-h-full overflow-y-scroll"></div> */}
-            <Routes path="/admin/*">
-                <Route path="/orders" element={<AdminOrdersPage />}></Route>
-                <Route path="/products" element={<AdminProductsPage />}></Route>
-                <Route path="/add-product" element={<AdminAddProductPage />}></Route>
-                <Route path="/update-product/" element={<AdminUpdateProductPage />}></Route>
-                <Route path="/users" element={<h1>Users Page</h1>}></Route>
-                <Route path="/reviews" element={<h1>Reviews Page</h1>}></Route>
-            </Routes>
-        </div>
-
-    </div>)
-    ;
+    useEffect(()=>{
+        const token = localStorage.getItem("token");
+        if(token == null){
+            window.location.href = "/";
+            return;
+        }
+        axios.get(import.meta.env.VITE_BACKEND_URL + "/users/", {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }).then((response)=>{
+            if(response.data.role == "admin"){
+                setUser(response.data);
+            }else{
+                window.location.href = "/";
+            }
+        }).catch(()=>{
+            window.location.href = "/login";
+        })
+    },[])
+	return (
+		<div className="w-full h-full flex bg-accent">
+            {user ?
+			<>
+				<div className="w-[300px] bg-accent h-full">
+					<div className="w-full h-[100px] flex items-center text-primary ">
+						<img src="/logo.png" className="h-full" />
+						<h1 className="text-2xl">Admin</h1>
+					</div>
+					<div className="w-full h-[400px] text-white text-2xl flex flex-col pl-[20px] pt-[20px]">
+						<Link
+							to="/admin"
+							className="w-full flex items-center h-[50px] gap-[10px]"
+						>
+							{" "}
+							<LuClipboardList />
+							Orders
+						</Link>
+						<Link
+							to="/admin/products"
+							className="w-full flex items-center h-[50px] gap-[10px]"
+						>
+							{" "}
+							<LuBoxes />
+							Products
+						</Link>
+						<Link
+							to="/admin/users"
+							className="w-full flex items-center h-[50px] gap-[10px]"
+						>
+							<FiUsers />
+							Users
+						</Link>
+					</div>
+				</div>
+				<div className="w-[calc(100%-300px)] h-full max-h-full bg-primary border-[10px] border-accent rounded-3xl overflow-y-scroll ">
+					<Routes>
+						<Route path="/" element={<AdminOrdersPage />} />
+						<Route path="/products" element={<AdminProductsPage />} />
+						<Route path="/add-product" element={<AdminAddProductPage />} />
+						<Route
+							path="/update-product"
+							element={<AdminUpdateProductPage />}
+						/>
+						<Route path="/users" element={<AdminUsersPage />} />
+						<Route path="/reviews" element={<h1>Reviews</h1>} />
+						
+					</Routes>
+				</div>
+			</>:
+            <Loader />
+}
+		</div>
+	);
 }
