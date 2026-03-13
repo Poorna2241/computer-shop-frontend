af5491666b20a7b3eef097c73a9bf3f023dd51e8
Poorna Sachinthana <poornasachinthana2241@gmail.com>
2026-03-13 15:46:41 +0530

    Add user data page

--- src/components/userData.jsx
+++ src/components/userData.jsx
@@ -1,10 +1,56 @@
-export default function UserData(props) {
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function UserData(){
+    const [user, setUser] = useState(null);
+    useEffect(()=>{
+        const token = localStorage.getItem("token");
+        if(token != null){
+            axios.get(import.meta.env.VITE_BACKEND_URL + "/users/", {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }).then((response)=>{
+                setUser(response.data);
+            }).catch(()=>{
+                setUser(null);
+            })
+        }
+    },[])
+    const [selectedOption, setSelectedOption] = useState("user");
+
     return (
-        <div>
-            <h2>User Information</h2>
-            <p>Name: {props.name}</p>
-            <p>Email: {props.email}</p>
-            <p>Age: {props.age}</p>
-        </div>
+        <>
+            {
+                user?
+                <div className="w-[150px] flex flex-row">
+                    <img src={user.image} referrerpolicy="no-referrer" className="w-[50px] rounded-full h-[50px]"/>
+                    <select className="bg-transparent outline-none ml-2 mt- text-white" value={selectedOption}
+                     onChange={
+                        (e)=>{
+                            
+                            if(e.target.value == "logout"){
+                                localStorage.removeItem("token");
+                                window.location.href = "/login";
+                            }else if(e.target.value == "my-orders"){
+                                window.location.href = "/orders";
+                            }
+                            setSelectedOption("user")
+                        }
+                    }>
+                        <option className="bg-accent" value={"user"}>{user.firstName}</option>
+                        <option className="bg-accent" value={"logout"}>Logout</option>
+                        <option className="bg-accent" value={"my-orders"}>My Orders</option>
+                    </select>
+                </div>:
+                <div className="w-[150px] flex flex-row">
+                    <Link to="/login" className="mx-2 px-4 py-2 bg-white text-accent rounded-full">Login</Link> /
+                    <Link to="/register" className="mx-2 px-4 py-2 bg-white text-accent rounded-full">Register</Link>
+                </div>
+            }
+        </>
     )
-} 
+
+
+}
